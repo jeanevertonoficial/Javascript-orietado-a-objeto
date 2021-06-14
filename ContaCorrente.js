995908100a1acadb68028fc5fe29073348ea214e
Jean Everton <jeanever39@gmail.com>
2021-06-14 15:41:50 -0300

    metodo get e set  no javascrip

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,10 +1,32 @@
+import { Cliente } from "./Cliente.js";
+
 // classe conta Corrente 
 export class ContaCorrente {
     agencia;
-    cliente;
+    _cliente;
+
+    // metodos acessores getter e setter, fazendo a proteção de dados
+    set cliente(novoValor) {
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+        
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+    
 
     _saldo = 0; // _saldo private ou #saldo para mostrar que este atributo é privado
 
+
+    // fazendo o retorno do saldo 
+    get saldo(){
+        return this._saldo;
+    }
+
+    
     // function de sacar
     sacar(valor) {
         if (this._saldo >= valor) {
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ cliente1.nome = "Jean";
 cliente1.cpf = 12345678910;
 cliente1.cidade = "Paiçandu";
 cliente1.rg = 12345652151;
+cliente1.agencia = 122;
 
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
@@ -14,11 +15,13 @@ cliente2.cpf = 12345678911;
 cliente2.cidade = "Maringá";
 cliente2.rg = 12345652;
 
+
 const cliente3 = new Cliente();
 cliente3.nome = "Everton";
 cliente3.cpf = 12345678913;
 cliente3.cidade = "São paulo";
 cliente3.rg = 12345652451;
+cliente3.agencia = 124;
 
 
 const contaCorrenteJean = new ContaCorrente();
@@ -35,8 +38,8 @@ contaCorrenteJean.depositar(1000);
 
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
+conta2.cliente = 0;
+conta2.agencia= 102;
 
 console.log(conta2);
 
@@ -47,8 +50,15 @@ console.log(conta2);
 
 //criando a conta apartir das classes
 const conta3 = new ContaCorrente();
-conta3.cliente = cliente3; // referente ao objeto cliente3 
-conta3.agencia = 103;
+
+conta3.cliente = new Cliente(); // referente ao objeto cliente3 
+conta3.cliente.nome = "Gustavo";  // fazendo atribuição direto 
+conta3.cliente.agencia = 103;
+conta3.cliente.cpf = 123456741;
+conta3.cliente.rg = 321654987;
+conta3.cliente.cidade = "Marialva";
+
+
 conta3.depositar(1500);
 conta3.depositar(3500);
 
@@ -61,7 +71,9 @@ console.log(conta3);
 // função de tranferencia de valor
 conta3.transferir(250, conta2);
 console.log(conta3);
-console.log(conta2);
+
+conta3.sacar(10);
+console.log(conta2.saldo);
 
 
 
@@ -70,3 +82,4 @@ console.log(conta2);
 // console.log(`CPF ${cliente1.cpf} \n`);
 // console.log(`Conta corrente ${cliente2.nome}`);
 // console.log(`CPF ${cliente2.cpf}`);
+
